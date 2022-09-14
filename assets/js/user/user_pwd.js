66f40b9f321245620c8a8f5a41afc921f06d97ba
zore <1459803771@qq.com>
2022-09-14 17:41:44 +0800

    增加user相关文件

---
+++ assets/js/user/user_pwd.js
@@ -0,0 +1,39 @@
+$(function () {
+    var form = layui.form
+
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        samePwd: function (value) {
+            if (value === $('[name = oldPwd]').val()) {
+                return '新旧密码不能相同！'
+            }
+        },
+        rePwd: function (value) {
+            if (value !== $('[name = newPwd]').val()) {
+                return '两次密码不一致！ '
+            }
+        }
+    })
+
+    // 给表单绑定一个提交事件
+    $('.layui-form').on('submit', function (e) {
+        // 阻止表单默认的提交行为
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新密码失败！')
+                }
+                layui.layer.msg('更新密码成功！')
+                // 重置表单
+                $('.layui-form')[0].reset()
+            }
+        })
+    })
+})
